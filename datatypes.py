3244b0b181496a3ffbd670b0a7668ac0229a9554
sambapython@gmail.com <sambapython@gmail.com>
2019-08-28 09:05:03 +0530

    2019-08-28 string operations.

---
+++ datatypes.py
@@ -0,0 +1,34 @@
+a=1000
+print(a,type(a))
+a=1.2
+print(a, type(a))
+a=1+2j
+print(a, type(a))
+a=[1,2,3,4,]
+print(a, type(a))
+a=(10,200,30,40)
+print(a,type(a))
+a=True
+print(a, type(a))
+a=False
+print(a,type(a))
+a=None
+print(a, type(a))
+a="python program"
+print(a, type(a))
+a={10,20,30,40}
+print(a, type(a))
+a={"name":"jay","age":2.3}
+print(a,type(a))
+# operators in python: +,-,*,/,//,**,%,>,>=,<,<=,==,!=,is,in,not,&,|,^,>>,<<
+a=1000
+b=2000
+print(a+b)
+print(a-b)
+a="python"
+b="program"
+print(a+b)
+a=100
+b="str"
+print(a+b)
+print(a-b)
---
+++ hello.py
@@ -0,0 +1,23 @@
+a=1000
+print(a,type(a))
+a=1.2
+print(a, type(a))
+a=1+2j
+print(a, type(a))
+a=[1,2,3,4,]
+print(a, type(a))
+a=(10,200,30,40)
+print(a,type(a))
+a=True
+print(a, type(a))
+a=False
+print(a,type(a))
+a=None
+print(a, type(a))
+a="python program"
+print(a, type(a))
+a={10,20,30,40}
+print(a, type(a))
+a={"name":"jay","age":2.3}
+print(a,type(a))
+# operators in python: +,-,*,/,//,**,%,>,>=,<,<=,==,!=,is,in,not,&,|,^,>>,<<
